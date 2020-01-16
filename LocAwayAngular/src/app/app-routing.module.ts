f9d1655398272b346114b2da6c3da728d436ccdb
GIPAI <jph.deuxguines@gmail.com>
2020-01-16 15:59:01 +0100

    bbb

--- LocAwayAngular/src/app/app-routing.module.ts
+++ LocAwayAngular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AccomodationComponent} from './accomodation/accomodation.component';
+import {AccomodationDetailComponent} from './accomodation-detail/accomodation-detail.component';
 
 
 const routes: Routes = [
   {path: 'accomodation', component: AccomodationComponent},
+  {path: 'accomodation-detail', component: AccomodationDetailComponent},
 ];
 
 @NgModule({
