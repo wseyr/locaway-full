e279a6dc0cc301bcef04dadd6c4dbac4bcfd4c7d
william <wseyrolle@gmail.com>
2020-01-16 16:35:31 +0100

    annotations crossorigin dans controleurs REST

--- locaway/src/main/java/com/sopra/LocAway/rest/OptionRestController.java
+++ locaway/src/main/java/com/sopra/LocAway/rest/OptionRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.sopra.LocAway.repository.IOptionRepository;
 
 @RestController
 @RequestMapping("/option")
+@CrossOrigin("*")
 public class OptionRestController {
 
 	@Autowired
