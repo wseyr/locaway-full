143d6e7c078028ff4a10a8bb898db853355b096a
william <wseyrolle@gmail.com>
2020-01-17 15:54:10 +0100

    Component accomodation-detail de base

--- LocAwayAngular/src/app/accomodation-detail/accomodation-detail.component.ts
+++ LocAwayAngular/src/app/accomodation-detail/accomodation-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AccomodationHttpService} from '../accomodation/accomodation-http.service';
 import {Accomodation} from '../Model/Accomodation';
+import {ActivatedRoute} from "@angular/router";
+import {NgbDate} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-accomodation-detail',
@@ -8,13 +10,28 @@ import {Accomodation} from '../Model/Accomodation';
   styleUrls: ['./accomodation-detail.component.css']
 })
 export class AccomodationDetailComponent implements OnInit {
-  accomodations: Array<Accomodation>;
+  accomodation_id: number;
+  accomodation: Accomodation = null;
 
-  constructor(private accomodationService: AccomodationHttpService) { }
+  //TODO: prendre images de la BDD
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-   findAll() : Array<Accomodation> {
-    return this.accomodationService.findAll();
-   }
+  //TODO: afficher les dates reservées dans le calendrier
+  //calendrier (en cours)
+  isDisabled = (date: NgbDate, current: {month: number}) => date.day === 13;
+
+
+  constructor(private route: ActivatedRoute, private accomodationService: AccomodationHttpService) {
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.accomodation_id = params['id'];
+      this.accomodationService.findById(this.accomodation_id).subscribe(resp => {
+        this.accomodation = resp;
+        console.log(resp);
+      });
+    });
+  }
+
   ngOnInit() {
   }
 
