6b22ebf41f2df043ee7b09d5fed062c7fdb6c628
flo <prieuflo@gmail.com>
2020-01-22 18:14:24 +0100

    oui

--- LocAwayAngular/src/app/dashboard/dashboard.component.ts
+++ LocAwayAngular/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../Model/User";
 import {UserHttpServiceService} from "../user/user-http-service.service";
+import {Accomodation} from '../Model/Accomodation';
+import {AccomodationHttpService} from '../accomodation/accomodation-http.service';
+import {Booking} from '../Model/Booking';
 
 @Component({
   selector: 'dashboard',
@@ -11,9 +14,46 @@ export class DashboardComponent implements OnInit {
   connectedU : User = JSON.parse(localStorage.getItem("connectedUser"));
   showreservation:boolean = false;
   showlocation:boolean = false;
-  constructor(private userService: UserHttpServiceService) {
+  accomodations: Array<Accomodation> = new Array<Accomodation>();
+
+  locations: Array<Accomodation> = new Array<Accomodation>();
+
+  constructor(private userService: UserHttpServiceService, private accomodationService: AccomodationHttpService) {
     console.log(this.connectedU);
   }
+
+  logements: Array<Accomodation> = new Array<Accomodation>();
+  bookings: Array<Booking> = new Array<Booking>();
+
+  listAll(){
+    return this.accomodationService.findAll();
+  }
+
+  listUserAcco(){
+    this.connectedU.accomodations.forEach((accomodation) =>{
+
+        this.accomodationService.findById(accomodation.id).subscribe((fullAcco) =>{
+          this.locations.push(fullAcco);
+        });
+      });
+
+    // for(let i=0;i  < this.listAll().length; i++)
+    //   if(this.listAll().includes(user.accomodations[i])){
+    //     this.logements.push(user.accomodations[i])
+    //   }
+    // return this.logements;
+  }
+/*  listUserAccoBooking(){
+    for(let logement of this.logements){
+      this.bookings = logement.bookings;
+    }
+  }*/
+
+/*  listedesbookings(user: User){
+    for(let accomodation of this.listUserAcco(user)){
+      result:Array<Booking> = accomodation.bookings;
+    }
+  }*/
   reservation(){
     this.showreservation = true;
     this.showlocation = false;
